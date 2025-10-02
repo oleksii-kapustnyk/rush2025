a242937f6682809f2cfcb1b1f6af44d862d8e4bb
Oleksii Kapustnyk <oleksii.kapustnyk@gmail.com>
2025-10-02 20:11:51 +0300

    test conflicts 2

--- src/com/ua/rush/lesson28/Main.java
+++ src/com/ua/rush/lesson28/Main.java
@@ -3,7 +3,7 @@ package com.ua.rush.lesson28;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test commit 0");
+        System.out.println("Test commit 0______");
         System.out.println("Test commit 2");
     }
 }
